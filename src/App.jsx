fe958cfd4ff972fe916c6a5d0fbf22e52539c524
leontoys <svliyon@gmail.com>
2025-03-10 12:17:08 +1100

    text-to-speech-init

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [text, setText] = useState("")
+  const [audioUrl,setAudioUrl] = useState("")
+
+  const handleConvert = async () => {
+    if (!text) {
+      alert("Please enter some text");
+      return; 
+    }
+  
+    try {
+      const response = await fetch(
+        '/api/models/facebook/fastspeech2-en-ljspeech', // TTS model
+        {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${import.meta.env.VITE_HUGGING_FACE_API_KEY}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ inputs: text }),
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error('Failed to do text to speech');
+      }
+  
+      const audioBlob = await response.blob();
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudioUrl(audioUrl);
+
+    } 
+    catch (error) {
+      console.error(error);
+      alert("Something went wrong. Please try again");
+    }
+  };
+
+  return (
+    <div className='App'>
+      <h1>Text to Speech</h1>
+      <textarea value={text}
+      onChange={(e)=>setText(e.target.value)}
+      placeholder='Enter your text here...'
+      ></textarea>
+      <button onClick={handleConvert}>Convert to Speech</button>
+      {audioUrl && (
+        <div>
+          <h2>Generated Speech</h2>
+          <audio controls src={audioUrl}></audio>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
